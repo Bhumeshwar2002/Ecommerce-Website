237184c6927f435bbab2be215f5b6827a7354257
Bhumeshwar2002 <chintanwarbhumeshwar@gmail.com>
2024-06-27 10:08:54 +0530

    Backend work almost done working on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,3 +30,26 @@ function App(){
 }
 
 export default App;
+
+
+// import React from 'react';
+// import './App.css';
+// import Header from './component/layout/Header/Header';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Footer from './component/layout/Footer/Footer';
+// import Home from './component/Home/Home';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -15,11 +15,44 @@ let initialState = {};
 const Middleware = [thunk];
 
 
-const store = configureStore(
+// const store = configureStore(
+//   reducer,
+//   initialState,
+  
+//   composeWithDevTools(applyMiddleware(...Middleware))
+// );
+const store = configureStore({
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...Middleware))
-);
-
+  Middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(Middleware),
+  devTools: composeWithDevTools(),
+  preloadedState: initialState
+});
+composeWithDevTools(applyMiddleware(...Middleware))
 export default store;
 
+
+// import { combineReducers, applyMiddleware } from "redux";
+// import { configureStore } from "@reduxjs/toolkit";
+// import {thunk} from 'redux-thunk'; // Import thunk correctly
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { productReducer } from "./reducers/productReducers";
+
+// const reducer = combineReducers({
+//   products: productReducer
+// });
+
+// let initialState = {};
+
+// const middleware = [thunk];
+
+// const store = configureStore({
+//   reducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(middleware),
+//   devTools: composeWithDevTools(),
+//   preloadedState: initialState
+// });
+
+// export default store;
